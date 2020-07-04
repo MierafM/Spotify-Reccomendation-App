2969acd4e25c59fa7c4a745805bc715387cfcace
unknown <mierafmulat@gmail.com>
2020-07-04 15:19:07 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//make components for a song
+//make compoents for table data
+
+let fakeServerData = {
+  user:{
+    name:'David'
+  },
+  recent: [
+    {
+      title:'song name1',
+      artist: 'the artist'
+    },
+    {
+      title:'song name2',
+      artist: 'the artist'
+    },
+    {
+      title:'song name3',
+      artist: 'the artist'
+    },
+    {
+      title:'song name4',
+      artist: 'the artist'
+    },
+    {
+      title:'song name5',
+      artist: 'the artist'
+    },
+    {
+      title:'song name6',
+      artist: 'the artist'
+    }
+  ]
+};
+class Song extends Component{
+  render(){
+    return(
+      <div style={{display:'inline-block', width:'30%'}}>
+        <img/>
+        <h4> {this.props.song.title}</h4>
+        <h5>{this.props.song.artist}</h5>
+      </div>
+    )
+  }
+}
+class Search extends Component{
+  render(){
+    return(
+      <div>
+        <img/>
+        <input type="text"/>Search
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({serverData: fakeServerData});
+    }, 1000);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>music app</h1>
+        </header>
+        <Search/>
+        {this.state.serverData.user &&
+          <h3> {this.state.serverData.user &&
+               this.state.serverData.user.name}'s Recently played</h3>
+         }
+        {this.state.serverData.user ?
+          <div className='Recent-songs'>
+          {this.state.serverData.recent.map(song =>
+            <Song song={song}/>
+          )}
+
+          </div> : <h4>no recently played</h4>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
